5e1695cb075107464872132af2b5095af94494b5
PurushothamanSrikanth <jispurushothaman@gmail.com>
2023-10-05 15:41:55 +0530

    Made changes to code - Dummy Data

--- dummy_data.py
+++ dummy_data.py
@@ -1,7 +1,11 @@
 ## This Python file would generate dummy data for the Data Warehouse
 
-import pandas, numpy, os, pyspark
+import pandas, numpy, os, sys, pyspark
 import time, random
+
+pathforDWH = 
+pathforDL = 
+
 
 # Creating a Spark Session
 spark = SparkSession\
@@ -10,8 +14,18 @@ spark = SparkSession\
     .enableHiveSupport()\
     .getOrCreate()
 
-# Schema
-schema = StructType(
+# Schema for DWH
+schema_for_DWH = StructType(
+    StructField("id", StringType(), False),
+    StructField("Name", StringType(), False),
+    StructField("Address", StringType(), False),
+    StructField("PAN", StringType(), False),
+    StructField("Phone_Number", IntergerType(), False),
+    StructField("isStudent", BooleanType(), False),
+    StructField("Salary", IntergerType(), False)
+)
+# Schema for DL
+schema_for_DL = StructType(
     StructField("id", StringType(), False),
     StructField("Name", StringType(), False),
     StructField("Address", StringType(), False),
@@ -20,10 +34,9 @@ schema = StructType(
     StructField("isStudent", StringType(), False),
     StructField("Salary", StringType(), False)
 )
-
 while True:
     try:
-        # Generate random data
+        # Generate random data for DWH
         data = [
             (
                 str(random.randint(1, 100)),
@@ -37,12 +50,32 @@ while True:
         ]
 
         # Create a DataFrame from the data and schema
-        df = spark.createDataFrame(data, schema= schema)
+        df = spark.createDataFrame(data, schema = schema_for_DWH)
 
-        # Writing data into the Hive table
-        df.write.mode("append").saveAsTable("user")
+        # Writing data into the Hive DataWarehouse table
+        df.write.mode("append").format("parquet").path(pathforDWH)
+        spark.sql('MSCK REPAIR TABLE test.users')
 
-        time.sleep(120) # Sleep for 2 minutes
+        # Generate random data for DL
+        data = [
+            (
+                str(random.randint(1, 100)),
+                "Name_" + str(random.randint(1, 10)),
+                "Address_"+ str(random.randint(1, 10)),
+                "PAN_" + str(random.randint(1, 10)),
+                str(random.randint(1000000000, 9999999999)),
+                random.choice(["TRUE", "FALSE"]),
+                str(random.randint(0, 99999)),
+            )
+        ]
+
+        # Create a DataFrame from the data and schema
+        df = spark.createDataFrame(data, schema = schema_for_DL)
+
+        # Writing data into the Hive DataWarehouse table
+        df.write.mode("append").format("parquet").path(pathforDL)
+
+        time.sleep(1)
     except KeyboardInterrupt:
         spark.stop()
         break
